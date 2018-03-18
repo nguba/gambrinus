2c5dec1651cd0f906025fe703b7bc163f357c11e
Nico Guba <nguba@me.com>
2018-03-18 22:34:55 +0000

    cleanup

--- src/main/java/me/nguba/gambrinus/domain/hardware/TemperatureSensor.java
+++ src/main/java/me/nguba/gambrinus/domain/hardware/TemperatureSensor.java
@@ -3,8 +3,6 @@ package me.nguba.gambrinus.domain.hardware;
 import me.nguba.gambrinus.domain.process.Event;
 import me.nguba.gambrinus.domain.process.Temperature;
 
-import java.util.EventListener;
-
 public interface TemperatureSensor<K, T> extends Sensor<K, T> {
 
   void onEvent(final Event<Temperature> event);
