7840a591dc3d2133457af28f121297a6bc3d50c5
Nico Guba <nguba@me.com>
2018-08-11 22:50:52 +0100

    started to work on vessels

---
+++ kernel/mash/src/main/java/me/nguba/gambrinus/process/MashTun.java
@@ -0,0 +1,24 @@
+package me.nguba.gambrinus.process;
+
+import me.nguba.gambrinus.ddd.Entity;
+
+/**
+ * @author <a href="mailto:nguba@mac.com">Nico Guba</a>
+ */
+public final class MashTun extends Entity<VesselId>
+{
+    private MashTun(VesselId id)
+    {
+        super(id);
+    }
+
+    public static MashTun create()
+    {
+        return new MashTun(VesselId.generate());
+    }
+
+    public static MashTun from(VesselId id)
+    {
+        return new MashTun(id);
+    }
+}
---
+++ kernel/mash/src/test/java/me/nguba/gambrinus/process/MashTunTest.java
@@ -0,0 +1,32 @@
+package me.nguba.gambrinus.process;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MashTunTest
+{
+    @Test
+    @DisplayName("create() generates a new unique identifier every time")
+    void createUsesUniqueIdentifier()
+    {
+        MashTun previous = MashTun.create();
+        
+        for(int idx = 0; idx < 100; idx++) {
+            MashTun current = MashTun.create();
+            assertThat(current).isNotEqualTo(previous);
+            
+            previous = current;
+        }
+    }
+    
+    @Test
+    void fromRecreatesPreviousState() {
+        MashTun previous = MashTun.create();
+        
+        MashTun current = MashTun.from(previous.getId());
+        
+        assertThat(current).isEqualTo(previous);
+    }
+}
